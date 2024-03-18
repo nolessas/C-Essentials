7c2ab04b785b954353e24994d363c87853c73cd3
nolessas <155294980+nolessas@users.noreply.github.com>
2024-03-18 19:14:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace CSVParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Razer\Desktop\data.csv";
+
+
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        Console.WriteLine($"Name: {values[0]}, Age: {values[1]}, Occupation: {values[2]}");
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file was not found: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
