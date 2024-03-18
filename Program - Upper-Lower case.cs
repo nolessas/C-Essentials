dc5778c066371b0ec24d278b4038ff57283fbb59
nolessas <155294980+nolessas@users.noreply.github.com>
2024-03-18 18:56:27 +0200

    Add files via upload

---
+++ Program - Upper-Lower case.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+using System.Globalization;
+
+namespace TitleCaseConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Full name");
+            string name = Console.ReadLine();
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            string titleCasename = myTI.ToTitleCase(name.ToLower());
+
+            Console.WriteLine($"Your name is {titleCasename}");
+            
+            Console.ReadKey();
+            
+        }
+    }
+}*/
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace DateFormatConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Date in MM/dd/yyyy");
+            string name = Console.ReadLine();
+
+            DateTime dateValue;
+            if (DateTime.TryParseExact(inputDate, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue))
+            {
+                string formattedDate = dateValue.ToString("dd-MM-yyyy");
+                Console.WriteLine($"Formatted date: {formattedDate}");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format.");
+            } 
+            Console.ReadKey();
+            
+        }
+    }
+}
