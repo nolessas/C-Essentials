c81c2dbf5c2eb0f74bbaa0d33862cf8ef2a9c2da
nolessas <155294980+nolessas@users.noreply.github.com>
2024-03-18 19:26:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+
+namespace JSONConverter 
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                Name = "John Doe",
+                Age = 30,
+                Occupation = "Software Developer"
+            };
+
+            string jsonString = JsonSerializer.Serialize(person);
+            Console.WriteLine(jsonString);
+        }
+    }
+}
