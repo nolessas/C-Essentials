ecc0724d8982178df5e6fbeeecefc2371d051505
nolessas <155294980+nolessas@users.noreply.github.com>
2024-03-18 19:02:17 +0200

    Update and rename Program.cs to Program-DateConvertion.cs

--- Program-DateConvertion.cs
+++ Program-DateConvertion.cs
@@ -1,28 +1,25 @@
-﻿using System;
+using System;
 using System.Globalization;
 
 namespace DateFormatConverter
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your Date in MM/dd/yyyy");
-            string name = Console.ReadLine();
+            Console.WriteLine("Enter a date in MM/dd/yyyy format:");
+            string inputDate = Console.ReadLine();
 
             DateTime dateValue;
             if (DateTime.TryParseExact(inputDate, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue))
             {
                 string formattedDate = dateValue.ToString("dd-MM-yyyy");
                 Console.WriteLine($"Formatted date: {formattedDate}");
-
             }
             else
             {
                 Console.WriteLine("Invalid date format.");
-            } 
-            Console.ReadKey();
-            
+            }
         }
     }
 }
